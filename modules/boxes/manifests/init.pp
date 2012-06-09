465f2ecace744b13e2f8298cf031995c268332f4
dazz <annejulia@gmail.com>
2012-06-09 16:22:39 -0400

    externalize globals to class boxes

--- modules/boxes/manifests/init.pp
+++ modules/boxes/manifests/init.pp
@@ -1,5 +1,18 @@
 class boxes {
 
+  $projectname       = "symfony"
+  $user              = "vagrant"
+  $www_group         = "www-data"
 
+  # dirs and folders
+  $install_dir       = "/var/www"
+  $project_root      = "$install_dir/$projectname"
 
+  # php_ini
+  $php_ini_timezone  ='Europe/Berlin'
+
+  # vhost config
+  $vhost_template    = 'apache/vhost-default.conf.erb'
+  $vhost_servername  = ''
+  $vhost_docroot     = "$project_root/web"
 }
