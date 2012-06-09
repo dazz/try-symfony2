cb7d6acc3c9c771a5a99d1fc8463a0089a3a98b0
dazz <annejulia@gmail.com>
2012-06-09 17:23:56 -0400

    minor edits

--- modules/boxes/manifests/init.pp
+++ modules/boxes/manifests/init.pp
@@ -13,15 +13,18 @@ class boxes {
   $user              = "vagrant"
   $www_group         = "www-data"
 
-  # dirs and folders
+  ## dirs and folders
   $install_dir       = "/var/www"
   $project_root      = "$install_dir/$projectname"
 
-  # php_ini
+  ## php_ini
   $php_ini_timezone  = 'Europe/Berlin'
 
-  # vhost config
+  ## vhost config
   $vhost_template    = 'apache/vhost-default.conf.erb'
   $vhost_servername  = ''
   $vhost_docroot     = "$project_root/web"
+
+  # testbox
+
 }
