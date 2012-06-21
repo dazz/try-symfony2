c915940f84ae561ee75ab92268ba27304af3041d
dazz <annejulia@gmail.com>
2012-06-21 20:59:55 +0200

    install jenkins

--- modules/boxes/manifests/cibox/jenkins.pp
+++ modules/boxes/manifests/cibox/jenkins.pp
@@ -7,34 +7,21 @@ class boxes::cibox::jenkins {
       key_source => "http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key",
     }
 
-#    define apt::source(
-#      $ensure            = present,
-#      $location          = '',
-#      $release           = $lsbdistcodename,
-#      $repos             = 'main',
-#      $include_src       = true,
-#      $required_packages = false,
-#      $key               = false,
-#      $key_server        = 'keyserver.ubuntu.com',
-#      $key_content       = false,
-#      $key_source        = false,
-#      $pin               = false
-#    ) {
-
     apt::source { "jenkins":
-      location          => "http://pkg.jenkins-ci.org/debian/",
-      key_content       => "deb http://pkg.jenkins-ci.org/debian binary/"
+      location          => "http://pkg.jenkins-ci.org/debian binary/",
+      release           => "",
+      include_src       => false,
+      repos             => "",
     }
 
     Apt::Key["jenkins"] -> Apt::Source["jenkins"]
 
-    package {"jenkins":
+    package { "jenkins":
         ensure  => "installed",
         require => Apt::Source["jenkins"],
-#        user    => root
     }
 
-    service {"jenkins":
+    service { "jenkins":
         enable  => true,
         ensure  => "running",
         hasrestart => true,
